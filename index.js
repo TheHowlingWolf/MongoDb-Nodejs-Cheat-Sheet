457aab866732002fc0beaca743b0111f0e298dfa
TheHowlingWolf <43552823+TheHowlingWolf@users.noreply.github.com>
2019-11-02 06:09:27 +0530

    5
    5

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ mongoose.connect('mongodb://localhost/playground',{useNewUrlParser:true,useUnifi
 //first property has the url of the mongodb database here we are using localhost and for real apps we get the link from MongoDb.com->login->Atlas->Sandbox->connect->Connect-Your-Application->connection-string-only->copy paste it in instead of localhost link 
 //Remember to change <password> in the pasted link with the user set password and enable whitelist ips recommended to use 0.0.0.0 and the second property is object with two attributes useNewUrlParser and useUnifiedTopology 
 //set them to true this is default syntax for Mongodb new release and enables to use new modules and overrules the depcricated ones!
+//Sometimes on connecting to online server many give an error named like:: ['Transient Transaction Error'] this can be resolved by changing your network connection like switching your wifi to hotspot or else vice versa
     .then(()=> console.log('Connected to MongoDB'))//if connected then show message
     .catch(()=> console.error('Could not Connect to Mongo Database',err)); //if connection failed then show message log error
 
